59ba9d68fbd4cbd692c3b3a2b1d742fbbc180451
TomasLukes <lukest@seznam.cz>
2023-05-23 22:30:56 +0200

    Import Arrow icon into ButtonArrow

--- src/app/components/buttons/ButtonArrow.tsx
+++ src/app/components/buttons/ButtonArrow.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image';
+import arrowIcon from '/public/icons/shared/icon-arrow-right.svg';
+
 export default function ButtonArrow({ label }) {
   return (
-    <button className="px-4 py-2 text-dark-900 opacity-50 font-bold
+    <button className="flex items-center gap-1 px-4 py-2 text-dark-900 opacity-50 font-bold
     hover:text-primary-700 hover:opacity-100">
-      {label} <i>{`>`}</i>
+      {label}
+      <Image src={arrowIcon} alt="Arrow Icon" className='inline' />
     </button>
   )
 }
