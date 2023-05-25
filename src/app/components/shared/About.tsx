070702617a02bda3a7bd181d705ed9df5046c67c
TomasLukes <lukest@seznam.cz>
2023-05-25 22:10:20 +0200

    Created About section, add initial styling

---
+++ src/app/components/shared/About.tsx
@@ -0,0 +1,25 @@
+
+export default function About() {
+  return (
+    <section id="about" className="container mx-auto flex flex-col-reverse lg:flex-row items-center gap:8
+      px-6">
+      {/* Text container */}
+      <div className="lg:w-1/2">
+        <h3 className="heading-2 text-center lg:text-left">
+          Bringing you the <br></br><span className="text-primary-700">best</span> audio gear
+        </h3>
+        <p className="text-body opacity-50 text-center lg:text-left">
+          Located at the heart of New York City, Audiophile is the premier store for high end headphones, 
+          earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration 
+          rooms available for you to browse and experience a wide range of our products. Stop by our store 
+          to meet some of the fantastic people who make Audiophile the best place to buy your portable 
+          audio equipment.
+        </p>
+      </div>
+      {/* Image container */}
+      <div className="lg:w-1/2">
+        <img src="/images/shared/image-best-gear.webp" alt="" className="lg:ml-auto rounded-lg " />
+      </div>
+    </section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link"
-import ButtonPrimary from "./components/buttons/ButtonPrimary"
-import ButtonSecondary from "./components/buttons/ButtonSecondary"
-import ButtonArrow from "./components/buttons/ButtonArrow"
+import About from "./components/shared/About"
+
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl pt-12 pb-6">Main page</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos blanditiis voluptatibus consequatur dicta, 
-        necessitatibus reiciendis cumque laudantium commodi sint mollitia unde est beatae aut optio atque molestiae
-        cum eligendi exercitationem.
-      </p>
+    <div>
+      <div className="container mx-auto">
+        <h1 className="text-3xl pt-12 pb-6">Main page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos blanditiis voluptatibus consequatur dicta, 
+          necessitatibus reiciendis cumque laudantium commodi sint mollitia unde est beatae aut optio atque molestiae
+          cum eligendi exercitationem.
+        </p>
+      </div>
+      <About />
     </div>
   )
 }
