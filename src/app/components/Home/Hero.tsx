b290499b9a884692a78b710d5f1e7a0fdc5ad2dc
TomasLukes <lukest@seznam.cz>
2023-05-26 21:57:06 +0200

    Optimize Hero section for all screen sizes

--- src/app/components/Home/Hero.tsx
+++ src/app/components/Home/Hero.tsx
@@ -5,28 +5,23 @@ export default function Hero() {
   return (
     /* Hero section */
     <section id="hero" className="bg-neutral-900 text-white
-        h-96 pb-12 px-6">
+        pb-12 md:px-6">
         {/* Hero container */}
-        <div className="container flex mx-auto 
+        <div className="container flex mx-auto text-center lg:text-left
             border-t border-white border-opacity-20">
             {/* Info container */}
-            <div className="w-1/2 py-32 px-6">
+            <div className="mx-auto lg:mx-0 lg:mr-auto lg:w-1/2 py-32 px-6">
                 <span className="overline-text opacity-25">
                     NEW PRODUCT
                 </span>
                 <h1 className="heading-1">
                     XX99 MARK II HEADPHONES
                 </h1>
-                <p className="w-1/2 text-body opacity-75 pt-6 pb-9">
+                <p className="lg>w-3/4 text-body opacity-75 pt-6 pb-9">
                     Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.    
                 </p>
                 <ButtonPrimary label={'SEE PRODUCT'} />
-
             </div>
-            {/* Image container */}
-            <div className="w-1/2">
-            </div>
-            
         </div>
     </section>
   )
