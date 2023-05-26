c56e35ecea965dbef0efea92e5039745c16714db
TomasLukes <lukest@seznam.cz>
2023-05-26 16:05:04 +0200

    Add menu icons, styled Header

--- src/app/components/navigation/Header.tsx
+++ src/app/components/navigation/Header.tsx
@@ -2,9 +2,39 @@ import Link from "next/link"
 
 export default function Header() {
   return (
-    <header className="w-full flex items-center justify-between py-12 px-24 bg-neutral-900 text-neutral-100">
-      <h1 className="text-3xl font-bold">Lynxes</h1>
-      <nav className="flex gap-6 list-none">
+    /* Header */
+    <header className="relative w-full bg-neutral-900 text-neutral-100">
+      {/* Header container */}
+      <div className="container mx-auto flex items-center justify-between py-12">
+        {/* Mobile menu */}
+        <img src="/icons/shared/menu/icon-menu-hamburger.svg" alt="" className="lg:hidden" />
+        {/* Logo */}
+        <h1 className="text-3xl font-bold">
+          Lynxes
+        </h1>
+        {/* Navbar items desktop */}
+        <nav className="hidden lg:flex gap-6 list-none">
+          <li>
+            <Link href="/category/earphones">Earphones</Link>
+          </li>
+          <li>
+            <Link href="/category/headphones">Headphones</Link>
+          </li>
+          <li>
+            <Link href="/category/speakers">Speakers</Link>
+          </li>
+          <li>
+          <Link href="/checkout">Checkout</Link>
+          </li>
+        </nav>
+        {/* Account + Shopping card icons */}
+        <div className="flex gap-2 items-center">
+          <img src="/icons/shared/icon-user-account.svg" alt="" className="fill-white"/>
+          <img src="/icons/shared/icon-cart.svg" alt="" />
+        </div>
+      </div>
+      {/* Navbar items desktop */}
+      <nav className="absolute w-full bg-red-600 flex gap-6 list-none">
         <li>
           <Link href="/category/earphones">Earphones</Link>
         </li>
@@ -18,11 +48,6 @@ export default function Header() {
         <Link href="/checkout">Checkout</Link>
         </li>
       </nav>
-
-      <div>
-        <img src="/icons/shared/icon-cart.svg" alt="" />
-      </div>
-
     </header>
   )
 }
