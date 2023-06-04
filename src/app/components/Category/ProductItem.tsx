ead85c0f3912cfcbb767bbd7fd288b80e968d07b
TomasLukes <lukest@seznam.cz>
2023-06-04 19:48:37 +0200

    Adding missing style props to ProductItem

--- src/app/components/Category/ProductItem.tsx
+++ src/app/components/Category/ProductItem.tsx
@@ -51,6 +51,7 @@ export default function ProductItem({ product }) {
         {/* Button */}
         <Link href={`product/${product.slug}`} className="text-light-100">
           <ButtonPrimary
+              style={'text-light-100'}
               label={'SEE PRODUCT'}
           />
         </Link>
